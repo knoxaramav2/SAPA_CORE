965f7f036cf7bf572792671755d7c0c42937fa4f
knoxaramav2 <wtscott@iastate.edu>
2024-02-19 11:07:01 -0600

    Notes

--- SapaCore/sapi.hpp
+++ SapaCore/sapi.hpp
@@ -6,6 +6,11 @@
 #define SAPICORE_API __declspec(dllimport)
 #endif
 
+/*
+Exposed API
+Intended to be used in language wrappers and direct use
+*/
+
 namespace SAPI {
 	extern "C" SAPICORE_API void test();
 }
